875456a975f17dd7fd0b194965bd8db3312fc83d
binduchowdary856 <binduchowdary856@gmail.com>
2020-11-11 13:45:38 +0530

    javascript files added

---
+++ html&css/task1.js
@@ -0,0 +1,9 @@
+    function menuBar(){
+         var x = document.getElementById("title2-id");
+            x.style.display = "block";
+    }
+    function wrongMenu(){
+        var x = document.getElementById("title2-id");
+        x.style.display = "none";
+
+    }
